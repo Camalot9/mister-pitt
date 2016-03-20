5e1333b8a2a8061df78bd31424005db374becea8
Cameron Hinkle <cameron.hinkle@nike.com>
2016-03-20 16:08:29 -0700

    Implemented front end with React.

--- data/repo.js
+++ data/repo.js
@@ -1,30 +1,127 @@
 var cassandra = require('cassandra-driver');
 var config = require('../config/config');
 var uuid = require('node-uuid');
+var async = require('async');
 
 var GET_ALL_QUERY = 'SELECT * FROM questionLookup';
 var GET_QUERY = 'SELECT * FROM questions WHERE id = ?';
 var INSERT_QUESTION_QUERY = 'INSERT INTO questions ( id, created, modified, removed, difficulty, question ) ' +
   ' VALUES ( ?, toTimestamp(now()), toTimestamp(now()), false, ?, ? )';
-var INSERT_LOOKUP_QUERY = 'INSERT INTO questionLookup ( difficulty, id ) VALUES ( ?, ? )';
+var INSERT_LOOKUP_QUERY = 'INSERT INTO questionLookup ( difficulty, id, created ) VALUES ( ?, ?, toTimestamp(now()))';
 var INSERT_ANSWER_QUERY = 'UPDATE questions SET answers[?] = ? WHERE id = ?';
 var MARK_ANSWER_CORRECT_QUERY = 'UPDATE questions SET correctAnswer = ?, modified = toTimestamp(now()) WHERE id = ?';
 var REMOVE_QUESTION_QUERY = 'UPDATE questions SET removed = true, modified = toTimestamp(now()) WHERE id = ?';
+var UPDATE_QUESTION_QUERY = 'UPDATE questions SET question = ?, difficulty = ?, modified = toTimestamp(now()) WHERE id = ?';
 
 var pinAnswerQuery = function(answerId) {
- return 'UPDATE questions SET pinnedAnswers = pinnedAnswers + [ ' + answerId + ' ] , modified = toTimestamp(now()) WHERE id = ?';
+ return 'UPDATE questions SET pinnedAnswers = pinnedAnswers + { ' + answerId + ' } , modified = toTimestamp(now()) WHERE id = ?';
+};
+
+var unpinAnswerQuery = function(answerId) {
+ return 'UPDATE questions SET pinnedAnswers = pinnedAnswers - { ' + answerId + ' } , modified = toTimestamp(now()) WHERE id = ?';
 };
 
 var removeAnswerQuery = function(answerId) {
-  return 'UPDATE questions SET removedAnswers = removedAnswers + [ ' + answerId + ' ] , modified = toTimestamp(now()) WHERE id = ?';
+  return 'UPDATE questions SET removedAnswers = removedAnswers + { ' + answerId + ' } , modified = toTimestamp(now()) WHERE id = ?';
 };
 
 var client = new cassandra.Client({ contactPoints: config.get('cassandra.nodes'), keyspace : config.get('cassandra.keyspace')});
 
+// Given an array of IDs and a single string ID, find out if the id is in there
+var containsUUID = function(id, ids) {
+  if (ids) {
+    for (var i=0; i < ids.length; i++) {
+      if (id === ids[i].toString()) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+var newQuestion = function(id, text, difficulty, removed) {
+  return {
+    id : id,
+    question : text,
+    difficulty : difficulty,
+    removed: removed,
+    answers : []
+  };
+};
+
+var newAnswer = function(id, text, correct, pinned) {
+  return {
+    id : id,
+    text : text,
+    correct : correct,
+    pinned : pinned
+  };
+};
+
+var updateResponse = function(success, message) {
+  return {
+    success : success,
+    message : message
+  };
+};
+
+// Given a Cassandra row describing a question, return a good looking question object
+var makeQuestionsObject = function(row) {
+  var question = newQuestion(row.id, row.question, row.difficulty, row.removed);
+  var answers = row.answers;
+  if (answers) {
+    for (var answerId in answers) {
+      // If the answer hasn't been removed, add it
+      if(answers.hasOwnProperty(answerId) && !containsUUID(answerId, row.removedAnswers)) {
+        question.answers.push(
+          newAnswer(
+            answerId, 
+            answers[answerId], 
+            !!(row.correctanswer && answerId === row.correctanswer.toString()), 
+            containsUUID(answerId, row.pinnedAnswers)
+          ));
+      }
+    }
+  }
+
+  return question;
+};
+
 var repo = {
-  getAllQuestions : function(callback) {
+  getAllQuestionsSummary : function(callback) {
     client.execute(GET_ALL_QUERY, function(err, result) {
-      callback(result);
+      callback(result.rows);
+    });
+  },
+
+  getQuestions : function(limit, afterQuestionsCallback) {
+    repo.getAllQuestionsSummary(function(rows) {
+      if (!rows || rows.length === 0) {
+        afterQuestionsCallback([]);
+        return;
+      }
+
+      var end = rows.length > limit ? limit : rows.length;
+      var rowsToGet = rows.slice(0, end);
+      var questions = []; 
+      async.each(
+        rowsToGet, 
+        function(item, afterEachRowCallback) {
+          repo.getQuestion(item.id, function(question) {
+            if (question) {
+              var questionObj = makeQuestionsObject(question);
+              //console.log(questionObj);
+              questions.push(questionObj);
+            }
+
+            // This tells async.js this iteration is all done
+            afterEachRowCallback();
+          });
+        }, 
+        function done(err) {
+          afterQuestionsCallback(questions);
+        }
+      );
     });
   },
 
@@ -34,8 +131,7 @@ var repo = {
         console.log(err);
         throw new Error(err.message);
       }
-
-      callback(result === undefined ? null : result.rows[0]);
+      callback(result ? result.rows[0] : null);
     });
   },
 
@@ -56,8 +152,19 @@ var repo = {
           console.log(err);
           throw new Error(err.message);
         }
-        callback(result);
+        callback(newQuestion(id, text, difficulty, false));
       });
+    });
+  },
+
+  updateQuestion : function(questionId, text, difficulty, callback) {
+    client.execute(UPDATE_QUESTION_QUERY, [text, difficulty, questionId], function(err, result) {
+      if (err) {
+        console.log(err);
+        callback(updateResponse(false, 'Failed updating quesiton ' + questionId + ': ' + err.message));
+      }
+
+      callback(updateResponse(true, 'Updated question ' + questionId));
     });
   },
 
@@ -87,7 +194,52 @@ var repo = {
         });
       }
 
-      callback(result);
+      callback(newAnswer(answerId, text, correct, pinned));
+    });
+  },
+
+  updateAnswerText : function(questionId, answerId, text, callback) {
+    // insert also updates
+    client.execute(INSERT_ANSWER_QUERY, [answerId, text, questionId], function(err, result) {
+      if (err) {
+        console.log(err);
+        callback(updateResponse(false, 'Failed updating answer ' + answerId + ': ' + err.message));
+      }
+
+      callback(updateResponse(true, 'Updated answer ' + answerId));
+    });
+  },
+
+  pinAnswer : function(questionId, answerId, callback) {
+    client.execute(pinAnswerQuery(answerId), [questionId], function(err, result) {
+      if (err) {
+        console.log(err);
+        callback(updateResponse(false, 'Failed pinning answer ' + answerId + ': ' + err.message));
+      }
+
+      callback(updateResponse(true, 'Pinned answer ' + answerId));
+    });
+  },
+
+  unpinAnswer : function(questionId, answerId, callback) {
+    client.execute(unpinAnswerQuery(answerId), [questionId], function(err, result) {
+      if (err) {
+        console.log(err);
+        callback(updateResponse(false, 'Failed unpinning answer ' + answerId + ': ' + err.message));
+      }
+
+      callback(updateResponse(true, 'Pinned unanswer ' + answerId));
+    });
+  },
+
+  markAnswerCorrect : function(questionId, answerId, callback) {
+    client.execute(MARK_ANSWER_CORRECT_QUERY, [answerId, questionId], function(err, result) {
+      if (err) {
+        console.log(err);
+        callback(updateResponse(false, 'Failed marking correct answer ' + answerId + ': ' + err.message));
+      }
+
+      callback(updateResponse(true, 'Marked correct answer ' + answerId));
     });
   },
 
@@ -95,21 +247,22 @@ var repo = {
     client.execute(removeAnswerQuery(answerId), [questionId], function(err, result) {
       if (err) {
         console.log(err);
-        throw new Error('Failed removing answer ' + answerId + ', message=' + err.message);
+        callback(updateResponse(false, 'Failed removing answer ' + answerId + ': ' + err.message));
       }
 
-      callback(result);
+      callback(updateResponse(true, 'Removed answer ' + answerId));
     });
   },
 
-  removeQuestion : function(questionId) {
+  removeQuestion : function(questionId, callback) {
+    console.log('removing question ' + questionId);
     client.execute(REMOVE_QUESTION_QUERY, [questionId], function(err, result) {
       if (err) {
         console.log(err);
-        throw new Error('Failed removing answer ' + answerId + ', message=' + err.message);
+        callback(updateResponse(false, 'Failed removing quesiton ' + questionId + ': ' + err.message));
       }
 
-      callback(result);
+      callback(updateResponse(true, 'Removed question ' + questionId));
     });
   }
 
--- server.js
+++ server.js
@@ -1,22 +1,50 @@
-var restify = require('restify');
-var getEndpoints = require('./handlers/get');
-var postEndpoints = require('./handlers/post');
+'use strict';
+const restify = require('restify');
+const getEndpoints = require('./handlers/get');
+const postEndpoints = require('./handlers/post');
+const deleteEndpoints = require('./handlers/delete');
+const putEndpoints = require('./handlers/put');
 
-var server = restify.createServer();
-server.use(restify.bodyParser({ mapParams: false }));
-server.get('/admin/questions', getEndpoints.allQuestions);
-server.get('/admin/questions/:questionId', getEndpoints.question);
-server.post('/admin/questions', postEndpoints.addQuestion);
-server.post('/admin/questions/:questionId/answers', postEndpoints.addAnswer);
+class MisterPitt {
+	constructor() {
+		var server = restify.createServer();
+		server.use(restify.bodyParser({ mapParams: false }));
+		server.get('/admin/questions', getEndpoints.allQuestions);
+		server.get('/admin/questions/summary', getEndpoints.questionsSummary);
+		server.get('/admin/questions/:questionId', getEndpoints.question);
+		server.post('/admin/questions', postEndpoints.addQuestion);
+		server.post('/admin/questions/:questionId/answers', postEndpoints.addAnswer);
+		server.put('/admin/questions/:questionId', putEndpoints.updateQuestion);
+		server.put('/admin/questions/:questionId/answers/:answerId', putEndpoints.updateAnswerText);
+		server.put('/admin/questions/:questionId/answers/:answerId/pinned', putEndpoints.pinAnswer);
+		server.put('/admin/questions/:questionId/answers/:answerId/correct', putEndpoints.updateAnswerCorrect);
+		server.del('/admin/questions/:questionId', deleteEndpoints.removeQuestion);
+		server.del('/admin/questions/:questionId/answers/:answerId', deleteEndpoints.removeAnswer);
+		server.del('/admin/questions/:questionId/answers/:answerId/pinned', deleteEndpoints.unpinAnswer);
 
-server.on('uncaughtException', function (req, res, route, error) {
-  console.log(error);
-});
+		server.get(/\/pitt\/(css|js|fonts|build)\/?.*/, restify.serveStatic({
+		  'directory': './public',
+		  'default': 'index.html'
+		}));
 
-process.on('uncaughtException', function(err) {
-  console.log(err);
-});
+		server.get(/\/pitt\/?.*/, restify.serveStatic({
+		  directory: __dirname,
+		  file: 'index.html'
+		}));
 
-server.listen(8080, function() {
-  console.log('%s listening at %s', server.name, server.url);
-});
+		server.on('uncaughtException', function (req, res, route, error) {
+		  console.log(error);
+		  res.send(500);
+		});
+
+		process.on('uncaughtException', function(err) {
+		  console.log(err);
+		  res.send(500);
+		});
+
+		server.listen(8080, function() {
+		  console.log('%s listening at %s', server.name, server.url);
+		});
+	}
+}
+new MisterPitt();
